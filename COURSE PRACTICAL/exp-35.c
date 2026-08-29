50a858fd97ee68da7c4d0d332b9584ef87db5cdc
192524238 <192524238.simats@saveetha.com>
2026-08-29 10:39:05 +0530

    Create exp-35.c

---
+++ COURSE PRACTICAL/exp-35.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main()
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(".");
+
+    if (dir == NULL)
+    {
+        printf("Unable to open directory\n");
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (entry->d_name[0] != '.')
+            printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+
+    return 0;
+}
